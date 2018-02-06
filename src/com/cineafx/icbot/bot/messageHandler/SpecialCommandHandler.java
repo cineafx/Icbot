6c3ad2a00bab967b463995ee1b4b001d7e6c4b57
cineafx <cineafx@gmail.com>
2018-02-06 15:10:48 +0100

    Made  dispay nicer values

--- src/com/cineafx/icbot/bot/messageHandler/SpecialCommandHandler.java
+++ src/com/cineafx/icbot/bot/messageHandler/SpecialCommandHandler.java
@@ -11,12 +11,29 @@ public class SpecialCommandHandler {
 	
 	public String botUpTime() {
 		long millis = (System.currentTimeMillis() - startuptime);
-		String uptime = String.format("%d days, %d hours, %d min, %d sec", TimeUnit.MILLISECONDS.toDays(millis),
-				TimeUnit.MILLISECONDS.toHours(millis) - TimeUnit.DAYS.toHours(TimeUnit.MILLISECONDS.toDays(millis)),
-				TimeUnit.MILLISECONDS.toMinutes(millis)
-						- TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
-				TimeUnit.MILLISECONDS.toSeconds(millis)
-						- TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+		String uptime;
+		
+		//Calcuate "full" units ... makes 330 seconds into 5 minutes, 30 seconds
+		long days = TimeUnit.MILLISECONDS.toDays(millis);
+		long hours = TimeUnit.MILLISECONDS.toHours(millis) - TimeUnit.DAYS.toHours(TimeUnit.MILLISECONDS.toDays(millis));
+		long minutes = TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis));
+		long seconds = TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis));
+		
+		//Only shows the required amount of units (leaves out units that are empty)
+		if (days == 0) {
+			if (hours == 0) {
+				if (minutes == 0) {
+					uptime = String.format("%d sec", seconds);
+				} else {
+					uptime = String.format("%d min, %d sec", minutes, seconds);
+				}
+			} else {
+				uptime = String.format("%d hours, %d min, %d sec", hours, minutes, seconds);
+			}
+		} else {
+			uptime = String.format("%d days, %d hours, %d min, %d sec", days, hours, minutes, seconds);
+		}
+				
 		return uptime;
 	}
 	
